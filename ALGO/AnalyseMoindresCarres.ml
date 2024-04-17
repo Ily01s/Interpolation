0729309d7a694563bc234e5722f0c85c79cca598
ily01s <riak200104@gmail.com>
2024-04-17 14:12:10 +0200

    test

--- ALGO/AnalyseMoindresCarres.ml
+++ ALGO/AnalyseMoindresCarres.ml
@@ -4,18 +4,19 @@ class analyseMoindresCarres points d = object (self)
   val mutable points = points
   val d = d
   
-  (* Calcul de la ligne de la matrice A pour un point donné *)
+  (* Calcul de la ligne de la matrice A pour un point donné, avec ordre correct des termes *)
   method private calculer_ligne_matrice (p : Point.point) =
     let x = p#get_x in
     let y = p#get_y in
-    let rec aux i j acc =
-      if i > d then acc
-      else if i + j > d then aux (i + 1) 0 acc
-      else aux i (j + 1) ((x ** float_of_int i) *. (y ** float_of_int j) :: acc)
-    in
-    Array.of_list (List.rev (aux 0 0 []))
-  
-  (* Construction de la matrice A *)
+    let terms = ref [] in
+    for i = d downto 0 do
+      for j = d - i downto 0 do
+        terms := (x ** float_of_int i) *. (y ** float_of_int j) :: !terms
+      done;
+    done;
+    Array.of_list (List.rev !terms)
+
+  (* Construction de la matrice A avec l'ordre correct des termes *)
   method private construireMatriceA =
     let terms_count = (d + 1) * (d + 2) / 2 in
     let matrix = Array.make_matrix (List.length points) terms_count 0. in
@@ -25,55 +26,41 @@ class analyseMoindresCarres points d = object (self)
     ) points;
     matrix
   
-  (* Construction du vecteur b (ici zéros car non spécifié autrement) *)
-  method private construireVecteurB =
-    (* Création d'un tableau pour stocker les valeurs de 'y' pour chaque point *)
-    let b_values = List.map (fun pt -> pt#get_y) points |> Array.of_list in
-    (* Transformation du tableau en une matrice colonne (8x1 dans votre cas) *)
-    Mat.of_array b_values 1 (Array.length b_values) |> Mat.transpose
   
-   
-  
-  (* Résolution du système A^TAx = A^Tb *)
   method resoudreSysteme =
     let a = Mat.of_arrays (self#construireMatriceA) in
-    let b = self#construireVecteurB in
     Printf.printf "Matrice a :\n";
       Owl.Mat.print a;
-      Printf.printf "Vecteur b :\n";
-      Owl.Mat.print b;
-      
     (* Fill b with values *)
-    
-    let a = Mat.of_arrays (self#construireMatriceA) in
-let b = self#construireVecteurB in
+    (* Calculer A^T A *)
+  let ata = Mat.(transpose a *@ a) in
+  Printf.printf "Matrice ata :\n";
+      Owl.Mat.print ata;
+  (* Utiliser SVD pour résoudre A^TA x = 0 *)
+  let _, s, vt = Linalg.D.svd ata in
 
 (* Imprimez les dimensions de 'a' et 'b' pour le débogage *)
 Printf.printf "Dimensions de 'a': %d x %d\n" (Mat.row_num a) (Mat.col_num a);
-Printf.printf "Dimensions de 'b': %d x %d\n" (Mat.row_num b) (Mat.col_num b);
 
-let at = Mat.transpose a in
-(* Imprimez les dimensions de 'at' pour le débogage *)
-Printf.printf "Dimensions de 'at': %d x %d\n" (Mat.row_num at) (Mat.col_num at);
+     let _, smallest_singular_value_index = Mat.min_i s in
+     let solution = Mat.col vt smallest_singular_value_index.(1) in
+     let coeffs = Mat.to_array solution in
+     let coeffs_list = Array.to_list coeffs in (* Separate step for clarity *)
+     let coeffs_triplets =
+      let rec aux acc i j =
+        if i > d then acc
+        else if i + j > d then aux acc (i + 1) 0
+        else
+          let index = (i * (d + 1)) - (i * (i - 1) / 2) + j in
+          if index >= List.length coeffs_list then acc
+          else
+            let coeff = List.nth coeffs_list index in
+            aux ((i, j, coeff) :: acc) i (j + 1)
+      in
+      List.rev (aux [] 0 0)
+    
+     in
+     coeffs_triplets
 
-let ata = Mat.dot at a in
-(* Imprimez les dimensions de 'ata' pour le débogage *)
-Printf.printf "Dimensions de 'ata': %d x %d\n" (Mat.row_num ata) (Mat.col_num ata);
 
-let atb = Mat.dot at b in
-(* Imprimez les dimensions de 'atb' pour le débogage *)
-Printf.printf "Dimensions de 'atb': %d x %d\n" (Mat.row_num atb) (Mat.col_num atb);
-
-
-
-    let x = Linalg.D.linsolve ata atb in
-    let coeffs = Mat.to_array x in
-    let coeffs_list = Array.to_list coeffs in (* Separate step for clarity *)
-    let coeffs_triplets = List.mapi (fun index coeff ->
-      let i = index / (d + 1)in 
-      let j = index mod (d + 1)in
-      (i, j, coeff))
-     coeffs_list 
-    in
-    coeffs_triplets
 end
--- ALGO/main.ml
+++ ALGO/main.ml
@@ -5,7 +5,7 @@ open Polynome
 
 
 
-let d = 1;;  (* Par exemple, pour une courbe quadratique *)
+let d = 3;;  (* Par exemple, pour une courbe quadratique *)
 
 let () =
   let points = lire_points "./points.csv" in  (* Assurez-vous que lire_points est accessible *)
@@ -15,3 +15,17 @@ let () =
   print_endline polynome#to_string
 
 
+  (*let generer_points_cercle h k r n =
+    let points = ref [] in
+    for i = 0 to n - 1 do
+      let theta = 2.0 *. Float.pi *. (float_of_int i) /. (float_of_int n) in
+      let x = h +. r *. cos(theta) in
+      let y = k +. r *. sin(theta) in
+      points := new point x y :: !points  (* Création d'objets point ici *)
+    done;
+    List.rev !points
+  
+  let () =
+    let h, k, r = 0., 0., 5. in  (* Centre (h, k) et rayon r du cercle *)
+    let n = 100 in  (* Nombre de points à générer *)*)
+  
